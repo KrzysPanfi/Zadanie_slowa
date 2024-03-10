c16b474aeb39a149fed016e63c53b768f067e673
Krzysztof <krzyspanfilewicz@gmail.com>
2024-03-10 11:51:07 +0100

    Sprawdzone

--- src/Main.java
+++ src/Main.java
@@ -10,44 +10,44 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         String filePath = "C:\\Users\\PC\\Desktop\\prace szkolne\\slowa.txt";
         ArrayList<String> lista_slow = Wczytaj_dane(filePath);
-        if(lista_slow!=null) {
+        if (lista_slow != null) {
             //zad 1
             System.out.println("Zad1: " + Zad1(lista_slow));
-
 
             //zad 2
 
             System.out.println("Zad2: " + Zad2(lista_slow));
 
             //zad3
-            for(String i:Zad3(lista_slow)){
+            for (String i : Zad3(lista_slow)) {
                 System.out.println(i);
             }
         }
-         }
 
-private static ArrayList<String> Zad3(ArrayList<String> lista_slow) {
-        ArrayList<String> Wynik=new ArrayList<String>();
-        Wynik.add("Zad 3: ");
-    for (String i : lista_slow) {
-        String[] slowa = i.split(" ");
-        char[] str1=slowa[0].toCharArray();
-        char[] str2=slowa[1].toCharArray();
-
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-    if(Arrays.equals(str1,str2)){
-     Wynik.add(i);
-}
     }
-    return Wynik;
-}
+
+    private static ArrayList<String> Zad3(ArrayList<String> lista_slow) {
+        ArrayList<String> Wynik = new ArrayList<String>();
+        Wynik.add("Zad3:");
+        for (String i : lista_slow) {
+            String[] slowa = i.split(" ");
+            char[] str1 = slowa[0].toCharArray();
+            char[] str2 = slowa[1].toCharArray();
+
+            Arrays.sort(str1);
+            Arrays.sort(str2);
+            if (Arrays.equals(str1, str2)) {
+                Wynik.add(i);
+            }
+        }
+        return Wynik;
+    }
 
     private static int Zad2(ArrayList<String> lista_slow) {
-        int count=0;
+        int count = 0;
         for (String s : lista_slow) {
             String[] slowa = s.split(" ");
             String slowo1 = slowa[0];
@@ -60,10 +60,10 @@ private static ArrayList<String> Zad3(ArrayList<String> lista_slow) {
     }
 
     private static int Zad1(ArrayList<String> lista_slow) {
-        int num=0;
-        for (String i: lista_slow) {
-            String[] slowa=i.split(" ");
-            for(int j=0 ;j<2;j++) {
+        int num = 0;
+        for (String i : lista_slow) {
+            String[] slowa = i.split(" ");
+            for (int j = 0; j < 2; j++) {
                 char[] slowa_char = slowa[j].toCharArray();
                 if (slowa_char[slowa_char.length - 1] == 'A') {
                     num++;
@@ -73,8 +73,8 @@ private static ArrayList<String> Zad3(ArrayList<String> lista_slow) {
         return num;
     }
 
-    private static ArrayList<String> Wczytaj_dane( String filePath) {
-        ArrayList<String> lista_slow=new ArrayList<String>();
+    private static ArrayList<String> Wczytaj_dane(String filePath) {
+        ArrayList<String> lista_slow = new ArrayList<String>();
         try {
 
 
@@ -89,12 +89,12 @@ private static ArrayList<String> Zad3(ArrayList<String> lista_slow) {
             while ((line = bufferedReader.readLine()) != null) {
                 lista_slow.add(line);
             }
-            
+
             bufferedReader.close();
             return lista_slow;
 
         } catch (IOException e) {
-            
+
             e.printStackTrace();
             return null;
         }
